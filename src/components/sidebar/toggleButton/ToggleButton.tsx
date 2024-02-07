90c907fcce053a1c045a0f3efdaec90c30befa9f
kh4leoni <mihakkila@gmail.com>
2024-02-07 18:44:42 +0200

    Fixes

--- src/components/sidebar/toggleButton/ToggleButton.tsx
+++ src/components/sidebar/toggleButton/ToggleButton.tsx
@@ -33,7 +33,7 @@ const ToggleButton = (props: Props) => {
           strokeLinecap="round"
           variants={{
             closed: { d: 'M 2 16.346 L 20 16.346' },
-            open: { d: 'M 3 2.5 L 17 16.436' },
+            open: { d: 'M 3 2.5 L 17 16.346' },
           }}
         />
       </svg>
